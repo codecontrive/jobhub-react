bfbf27e9b29a58130629f9966555b4635d52b943
Dan <61497565+VadimVoDa@users.noreply.github.com>
2020-02-27 11:07:51 +0200

    Created 3 buttons (red, blue, white-grey) for each style: ButtonFull & ButtonOutline

--- reusable/buttons/ButtonOutline.js
+++ reusable/buttons/ButtonOutline.js
@@ -5,6 +5,18 @@ const styles = {
     fontSize: 16,
     padding: 10
   }
+  buttonOutlineBlue:{
+    border: '1px solid blue',
+    background: 'none',
+    fontSize: 16,
+    padding: 10
+  }
+  buttonOutlineWhite:{
+    border:'1px solid grey',
+    background: 'none',
+    fontSize: 16,
+    padding: 10
+  }
 };
 
 export const ButtonOutline = ({ title = "Button" }) => {
