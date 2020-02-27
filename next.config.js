130811e4c8894db64993679883679195ae6b6582
Horea <h22ea@icloud.com>
2020-02-28 00:01:58 +0200

    updated config file ⚙

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-const withCss = require("@zeit/next-css");
 
-module.exports = withCss({
-  cssModules: true
-});
+const withCSS = require('@zeit/next-css')
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+    [optimizedImages, {
+        handleImages: ['jpeg', 'png'],
+    }],
+    [withCSS, {
+      cssModules: true,
+    }],
+    
+]);
