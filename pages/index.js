2bcda8a3eb37e278d7f943da1e71b7c9d8a326d5
Robert San <robertsandru96@gmail.com>
2020-02-27 13:03:22 +0200

    Removed useless import

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 import { Layout } from "../reusable/layout/Layout";
 
 import { Button } from "../reusable/buttons/Button";
-import {
-  BUTTON_TYPE_FULL,
-  BUTTON_TYPE_OUTLINE
-} from "../constants/buttons/ButtonTypes";
+import { BUTTON_TYPE_OUTLINE } from "../constants/buttons/ButtonTypes";
 import { BUTTON_SCHEMA_DANGER } from "../constants/buttons/ButtonColorSchemas";
 
 const Main = () => (
