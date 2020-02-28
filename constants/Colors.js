ffabdc310e5571bebbab220a3cabd3aff600dde1
Robert San <robertsandru96@gmail.com>
2020-02-28 22:58:40 +0200

    Colors.js - defined schema and objects

---
+++ constants/Colors.js
@@ -0,0 +1,32 @@
+export const MainColor = {
+  Red: "#FF0000",
+  Orange: "#FF7F00",
+  Yellow: "#FFFF00",
+  Green: "#00FF00",
+  Blue: "#0000FF",
+  Indigo: "#4B0082",
+  Violet: "#9400D3"
+};
+
+export const Tone = {
+  Red: {
+    LightRed1: "hex",
+    LightRed2: "hex",
+    LightRed3: "hex",
+    LightRed4: "hex",
+    DarkRed1: "hex",
+    DarkRed2: "hex",
+    DarkRed3: "hex",
+    DarkRed4: "hex"
+  }
+};
+
+export const SystemColor = {
+  Icon: MainColor.Blue
+};
+
+export default {
+  MainColor: MainColor,
+  Tone: Tone,
+  SystemColor: SystemColor
+};
