f85698e095b60c730dbe0eb56de5ca9bf9464274
Robert San <robertsandru96@gmail.com>
2020-02-29 13:59:26 +0200

    Removed yet another useless import

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import jobReducer, { initialState as jobState } from "./jobReducers";
+import jobReducer from "./jobReducers";
 
 export default combineReducers({
   jobs: jobReducer
